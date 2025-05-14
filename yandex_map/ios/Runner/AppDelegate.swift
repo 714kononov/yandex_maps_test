f777687907496a44ab910f8f26e9e173fbc5853d
714kononov <lynxdrugs@gmail.com>
2025-05-14 18:02:10 +0700

    review

--- yandex_map/ios/Runner/AppDelegate.swift
+++ yandex_map/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import YandexMapsMobile
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey("2aebb2e1-6862-44d8-90e6-c4d9ae677400")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
